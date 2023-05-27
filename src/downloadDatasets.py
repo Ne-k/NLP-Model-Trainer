6aadb6ea857433a8dba88093a1b2ba4455005b38
Nguyen <68248147+Ne-k@users.noreply.github.com>
2023-05-27 02:31:03 -0700

    Update

--- src/downloadDatasets.py
+++ src/downloadDatasets.py
@@ -1,9 +1,11 @@
 import json
 from datasets import load_dataset
+import os
 
+os.makedirs('./datasets', exist_ok=True)
 gpt4 = load_dataset('vicgalle/alpaca-gpt4')
 
-with open('../datasets/gpt4.json', 'a') as f:
+with open('./datasets/gpt4.json', 'a') as f:
     f.write('[\n')
     for i, example in enumerate(gpt4['train']):
         input_text = example['instruction'] + " " + example['input']
@@ -16,7 +18,7 @@ with open('../datasets/gpt4.json', 'a') as f:
 
 long_form_data = load_dataset('akoksal/LongForm')
 
-with open('../datasets/LongForm.json', 'a') as f:
+with open('./datasets/LongForm.json', 'a') as f:
     f.write('[\n')
     for i, example in enumerate(long_form_data['train']):
         input_text = example['input']
@@ -27,22 +29,22 @@ with open('../datasets/LongForm.json', 'a') as f:
             f.write(',\n')
     f.write('\n]')
 
-long_form_data = load_dataset('akoksal/LongForm')
+long_form_data_val = load_dataset('akoksal/LongForm')
 
-with open('../datasets/validate-LongForm.json', 'a') as f:
+with open('./datasets/validate-LongForm.json', 'a') as f:
     f.write('[\n')
-    for i, example in enumerate(long_form_data['validation']):
+    for i, example in enumerate(long_form_data_val['validation']):
         input_text = example['input']
         target_text = example['output']
         data = {'input_text': input_text, 'target_text': target_text}
         json.dump(data, f, indent=4)
-        if i != len(long_form_data['validation']) - 1:
+        if i != len(long_form_data_val['validation']) - 1:
             f.write(',\n')
     f.write('\n]')
 
 gsm8k_data = load_dataset('gsm8k', "main")
 
-with open('../datasets/validate-gsm8k.json', 'a') as f:
+with open('./datasets/validate-gsm8k.json', 'a') as f:
     f.write('[\n')
     for i, example in enumerate(gsm8k_data['test']):
         input_text = example['question']
@@ -53,36 +55,36 @@ with open('../datasets/validate-gsm8k.json', 'a') as f:
             f.write(',\n')
     f.write('\n]')
 
-codex_math_qa_data = load_dataset('theblackcat102/codex-math-qa')
+codex_math_qa_data_val = load_dataset('theblackcat102/codex-math-qa')
 
-with open('../datasets/validate-Codex.json', 'a') as f:
+with open('./datasets/validate-Codex.json', 'a') as f:
     f.write('[\n')
-    for i, example in enumerate(codex_math_qa_data['validation']):
+    for i, example in enumerate(codex_math_qa_data_val['validation']):
         input_text = example['question']
         target_text = example['reply']
         data = {'input_text': input_text, 'target_text': target_text}
         json.dump(data, f, indent=4)
-        if i != len(codex_math_qa_data['validation']) - 1:
+        if i != len(codex_math_qa_data_val['validation']) - 1:
             f.write(',\n')
     f.write('\n]')
 
-codex_math_qa_rational_data = load_dataset('theblackcat102/codex-math-qa', "rational")
+codex_math_qa_rational_data_val = load_dataset('theblackcat102/codex-math-qa', "rational")
 
-with open('../datasets/validate-CodexRational.json', 'a') as f:
+with open('./datasets/validate-CodexRational.json', 'a') as f:
     f.write('[\n')
-    for i, example in enumerate(codex_math_qa_rational_data['validation']):
+    for i, example in enumerate(codex_math_qa_rational_data_val['validation']):
         input_text = example['question']
         target_text = example['reply']
         data = {'input_text': input_text, 'target_text': target_text}
         json.dump(data, f, indent=4)
-        if i != len(codex_math_qa_rational_data['validation']) - 1:
+        if i != len(codex_math_qa_rational_data_val['validation']) - 1:
             f.write(',\n')
     f.write('\n]')
 
-
+# Currently broken, I'll maybe fix it
 math = load_dataset('competition_math')
 
-with open('../datasets/Competition_math.json', 'a') as f:
+with open('./datasets/Competition_math.json', 'a') as f:
     f.write('[\n')
     for i, example in enumerate(math['train']):
         input_text = example['problem']
@@ -93,64 +95,63 @@ with open('../datasets/Competition_math.json', 'a') as f:
             f.write(',\n')
     f.write('\n]')
 
-math = load_dataset('competition_math')
+math_val = load_dataset('competition_math')
 
-with open('../datasets/validate-Competition_math.json', 'a') as f:
+with open('./datasets/validate-Competition_math.json', 'a') as f:
     f.write('[\n')
-    for example in math['test']:
+    for i, example in enumerate(math_val['test']):
         input_text = example['problem']
         target_text = example['solution']
         data = {'input_text': input_text, 'target_text': target_text}
         json.dump(data, f, indent=4)
-        if i != len(math['test']) - 1:
+        if i != len(math_val['test']) - 1:
             f.write(',\n')
     f.write(']\n')
-
 piqa = load_dataset('piqa')
 
-with open('../datasets/validate-Piqa.json', 'a') as f:
+with open('./datasets/validate-Piqa.json', 'w') as f:
     f.write('[\n')
-    for example in piqa['validation']:
+    for i, example in enumerate(piqa['validation']):
         input_text = example['goal']
         target_text = example['sol1']
         data = {'input_text': input_text, 'target_text': target_text}
         json.dump(data, f, indent=4)
         if i != len(piqa['validation']) - 1:
             f.write(',\n')
-    f.write(']\n')
+    f.write('\n]\n')
 
 human_eval_data = load_dataset('openai_humaneval')
 
-with open('../datasets/OpenAI_HumanEval.json', 'a') as f:
+with open('./datasets/OpenAI_HumanEval.json', 'a') as f:
     f.write('[\n')
-    for example in human_eval_data['test']:
+    for i, example in enumerate(human_eval_data['test']):
         input_text = example['prompt']
         target_text = example['canonical_solution']
         data = {'input_text': input_text, 'target_text': target_text}
         json.dump(data, f, indent=4)
         if i != len(human_eval_data['test']) - 1:
             f.write(',\n')
+    f.write('\n]')
 
-    f.write(']\n')
 
-gsm8k_data = load_dataset('gsm8k', "main")
+gsm8k_data_val = load_dataset('gsm8k', 'main')
 
-with open('../datasets/gsm8k.json', 'a') as f:
+with open('./datasets/gsm8k.json', 'w') as f:
     f.write('[\n')
-    for example in gsm8k_data['train']:
+    for i, example in enumerate(gsm8k_data_val['train']):
         input_text = example['question']
         target_text = example['answer']
         data = {'input_text': input_text, 'target_text': target_text}
         json.dump(data, f, indent=4)
-        if i != len(gsm8k_data['train']) - 1:
+        if i != len(gsm8k_data_val['train']) - 1:
             f.write(',\n')
-    f.write(']\n')
+    f.write('\n]')
 
 codex_math_qa_data = load_dataset('theblackcat102/codex-math-qa')
 
-with open('../datasets/Codex.json', 'a') as f:
+with open('./datasets/Codex.json', 'a') as f:
     f.write('[\n')
-    for example in codex_math_qa_data['train']:
+    for i, example in enumerate(codex_math_qa_data['train']):
         input_text = example['question']
         target_text = example['reply']
         data = {'input_text': input_text, 'target_text': target_text}
@@ -159,109 +160,88 @@ with open('../datasets/Codex.json', 'a') as f:
             f.write(',\n')
     f.write(']\n')
 
-codex_math_qa_rational_data = load_dataset('theblackcat102/codex-math-qa', "rational")
+codex_math_qa_rational_data = load_dataset('theblackcat102/codex-math-qa', 'rational')
 
-with open('../datasets/CodexRational.json', 'a') as f:
+with open('./datasets/CodexRational.json', 'w') as f:
     f.write('[\n')
-    for example in codex_math_qa_rational_data['test']:
+    for i, example in enumerate(codex_math_qa_rational_data['test']):
         input_text = example['question']
         target_text = example['reply']
         data = {'input_text': input_text, 'target_text': target_text}
         json.dump(data, f, indent=4)
         if i != len(codex_math_qa_rational_data['test']) - 1:
             f.write(',\n')
-    f.write(']\n')
+    f.write('\n]')
 
-math = load_dataset('competition_math')
-
-with open('../datasets/Competition_math.json', 'a') as f:
-    f.write('[\n')
-    for example in math['test']:
-        input_text = example['problem']
-        target_text = example['solution']
-        data = {'input_text': input_text, 'target_text': target_text}
-        json.dump(data, f, indent=4)
-        if i != len(math['test']) - 1:
-            f.write(',\n')
-    f.write(']\n')
 
 spell = load_dataset('vishnun/SpellGram')
 
-with open('../datasets/SpellGram.json', 'a') as f:
+with open('./datasets/SpellGram.json', 'a') as f:
     f.write('[\n')
-    for example in spell['train']:
+    for i, example in enumerate(spell['train']):
         input_text = example['source']
         target_text = example['target']
         data = {'input_text': input_text, 'target_text': target_text}
         json.dump(data, f, indent=4)
         if i != len(spell['train']) - 1:
             f.write(',\n')
-    f.write(']\n')
-
-wikipedia_data = load_dataset('wikipedia', '20220301.en')
-
-file_num1 = 1
-counter1 = 0
-f = open(f'../datasets/Wikipedia_{file_num1}.json', 'a')
-f.write('[\n')
-
-for example in wikipedia_data['train']:
-    input_text = example['title']
-    target_text = example['text']
-    data = {'input_text': input_text, 'target_text': target_text}
-    json.dump(data, f, indent=4)
-    if i != len(wikipedia_data['train']) - 1:
-        f.write(',\n')
-
-    counter1 += 1
-    if counter1 == 250:
-        f.write(']\n')
-        f.close()
-        file_num1 += 1
-        f = open(f'../datasets/Wikipedia{file_num1}.json', 'a')
-        f.write('[\n')
-        counter1 = 0
-
-f.write(']\n')
-f.close()
+    f.write('\n]')
 
 piqa = load_dataset('piqa')
 
-with open('../datasets/Piqa.json', 'a') as f:
+with open('./datasets/Piqa.json', 'w') as f:
     f.write('[\n')
-    for example in piqa['train']:
+    for i, example in enumerate(piqa['train']):
         input_text = example['goal']
         target_text = example['sol1']
         data = {'input_text': input_text, 'target_text': target_text}
         json.dump(data, f, indent=4)
         if i != len(piqa['train']) - 1:
             f.write(',\n')
-    f.write(']\n')
+    f.write('\n]\n')
 
 wiki_qa = load_dataset('wiki_qa')
 
-file_num = 1
-counter = 0
-f = open(f'../datasets/Wiki_qa_{file_num}.json', 'a')
-f.write('[\n')
+with open('./datasets/Wiki_qa.json', 'w') as f:
+    f.write('[\n')
+    for i, example in enumerate(wiki_qa['train']):
+        input_text = example['question']
+        target_text = example['answer']
+        data = {'input_text': input_text, 'target_text': target_text}
+        json.dump(data, f, indent=4)
+        if i != len(wiki_qa['train']) - 1:
+            f.write(',\n')
+    f.write('\n]\n')
 
-for example in wiki_qa['train']:
-    input_text = example['question']
-    target_text = example['answer']
+wikipedia_data = load_dataset('wikipedia', '20220301.en')
+
+file_num = 0
+file_path = f'./datasets/Wikipedia_{file_num}.json'
+file_count = 0
+
+for i, example in enumerate(wikipedia_data['train']):
+    if file_count >= 500000: # I'm just throwing random numbers here because I don't want to calculate the math.
+        # 20000 = 199 files.
+        break
+    if i % 100 == 0:
+        file_num = i // 100
+        file_path = f'./datasets/Wikipedia_{file_num}.json'
+        with open(file_path, 'w') as f:
+            f.write('[\n')
+    input_text = example['title']
+    target_text = example['text']
     data = {'input_text': input_text, 'target_text': target_text}
-    json.dump(data, f, indent=4)
-    f.write(',\n')
-
-    counter += 1
-    if counter == 250:
-        f.write(']\n')
-        f.close()
-        file_num += 1
-        f = open(f'../datasets/Wiki_qa_{file_num}.json', 'a')
-        f.write('[\n')
-        counter = 0
-
-f.write(']\n')
-f.close()
+    with open(file_path, 'a') as f:
+        json.dump(data, f, indent=4)
+        if i != len(wikipedia_data['train']) - 1 and (i + 1) % 100 != 0:
+            f.write(',\n')
+        elif i != len(wikipedia_data['train']) - 1:
+            f.write('\n]\n')
+        file_count += 1
+            
+# What the fuck? 
+# if i % 100 != 99:
+#     with open(file_path, 'a') as f:
+#         f.write('\n]\n')
 
 print("Done saving datasets")
--- src/trainer.py
+++ src/trainer.py
@@ -4,7 +4,7 @@ import tempfile
 import torch
 import re
 
-from torch.utils.data import ConcatDataset
+from torch.utils.data import ConcatDataset, DataLoader
 from transformers import T5ForConditionalGeneration, T5Tokenizer, Trainer, TrainingArguments
 
 
@@ -32,63 +32,74 @@ tokenizer = T5Tokenizer.from_pretrained('t5-base')
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = T5ForConditionalGeneration.from_pretrained('t5-base').to(device)
 
-with tempfile.NamedTemporaryFile(mode='w+', delete=True) as temp_file:
-    for filename in os.listdir('../datasets'):
-        if not re.search(r'validate-', filename):
-            with open(f'../datasets/{filename}', 'r') as f:
-                for item in json.load(f):
-                    if 'input_text' in item and 'target_text' in item:
-                        temp_file.write(json.dumps(item) + '\n')
-    temp_file.seek(0)
-    train_data = [json.loads(line) for line in temp_file]
+with tempfile.TemporaryDirectory() as temp_dir:
+    temp_file_path = os.path.join(temp_dir, 'train_data.json')
+    with open(temp_file_path, 'w+') as temp_file:
+        for filename in os.listdir('./datasets'):
+            if not re.search(r'validate-', filename):
+                with open(f'./datasets/{filename}', 'r') as f:
+                    try:
+                        for item in json.load(f):
+                            if 'input_text' in item and 'target_text' in item:
+                                temp_file.write(json.dumps(item) + '\n')
+                    except json.JSONDecodeError:
+                        print(f"Error loading file: {filename}")
+        temp_file.seek(0)
+        train_data = [json.loads(line) for line in temp_file]
 
-train_encodings = tokenizer([item['input_text'] for item in train_data if isinstance(item['input_text'], str)],
+    train_encodings = tokenizer([item['input_text'] for item in train_data if isinstance(item['input_text'], str)],
+                                return_tensors='pt', padding=True,
+                                truncation=True)
+    train_labels = tokenizer([item['target_text'] for item in train_data if isinstance(item['target_text'], str)],
+                             return_tensors='pt', padding=True,
+                             truncation=True)
+
+    train_dataset = TextGenerationDataset(train_encodings, train_labels)
+
+    temp_file_path = os.path.join(temp_dir, 'eval_data.json')
+    with open(temp_file_path, 'w+') as temp_file:
+        for filename in os.listdir('./datasets'):
+            if re.search(r'validate-', filename):
+                with open(f'./datasets/{filename}', 'r') as f:
+                    try:
+                        for item in json.load(f):
+                            if 'input_text' in item and 'target_text' in item:
+                                temp_file.write(json.dumps(item) + '\n')
+                    except json.JSONDecodeError:
+                        print(f"Error loading file: {filename}")
+        temp_file.seek(0)
+        eval_data = [json.loads(line) for line in temp_file]
+
+    eval_encodings = tokenizer([item['input_text'] for item in eval_data if isinstance(item['input_text'], str)],
+                               return_tensors='pt', padding=True,
+                               truncation=True)
+    eval_labels = tokenizer([item['target_text'] for item in eval_data if isinstance(item['target_text'], str)],
                             return_tensors='pt', padding=True,
                             truncation=True)
-train_labels = tokenizer([item['target_text'] for item in train_data if isinstance(item['target_text'], str)],
-                         return_tensors='pt', padding=True,
-                         truncation=True)
 
-train_dataset = TextGenerationDataset(train_encodings, train_labels)
+    eval_dataset = TextGenerationDataset(eval_encodings, eval_labels)
 
-with tempfile.NamedTemporaryFile(mode='w+', delete=True) as temp_file:
-    for filename in os.listdir('../datasets'):
-        if re.search(r'validate-', filename):
-            with open(f'../datasets/{filename}', 'r') as f:
-                for item in json.load(f):
-                    if 'input_text' in item and 'target_text' in item:
-                        temp_file.write(json.dumps(item) + '\n')
-    temp_file.seek(0)
-    eval_data = [json.loads(line) for line in temp_file]
+    eval_dataset_combined = ConcatDataset([train_dataset, eval_dataset])
 
-eval_encodings = tokenizer([item['input_text'] for item in eval_data if isinstance(item['input_text'], str)],
-                           return_tensors='pt', padding=True,
-                           truncation=True)
-eval_labels = tokenizer([item['target_text'] for item in eval_data if isinstance(item['target_text'], str)],
-                        return_tensors='pt', padding=True,
-                        truncation=True)
+    train_dataloader = DataLoader(train_dataset, batch_size=8, shuffle=True)
+    eval_dataloader = DataLoader(eval_dataset_combined, batch_size=8)
 
-eval_dataset = TextGenerationDataset(eval_encodings, eval_labels)
+    training_args = TrainingArguments(
+        output_dir='../results',
+        evaluation_strategy='epoch',
+        logging_dir='../logs',
+        per_device_train_batch_size=1,
+        num_train_epochs=100,
+    )
 
-eval_dataset_combined = ConcatDataset([train_dataset, eval_dataset])
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataloader=train_dataloader,
+        eval_dataloader=eval_dataloader
+        # save_strategy="no"
+    )
 
+    trainer.train()
 
-training_args = TrainingArguments(
-    output_dir='../results',
-    evaluation_strategy='epoch',
-    logging_dir='../logs',
-    per_device_train_batch_size=1,
-    num_train_epochs=100,
-)
-
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=train_dataset,
-    eval_dataset=eval_dataset_combined
-    # save_strategy="no"
-)
-
-trainer.train()
-
-model.save_pretrained('../trained_model')
+    model.save_pretrained('./trained_model')
