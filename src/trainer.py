0df1748c9367f54c1fa0428b21b887828e8ee80a
Nek <nekk.ng3@gmail.com>
2023-06-01 07:51:44 -0800

    Removed wiki file limit

--- src/trainer.py
+++ src/trainer.py
@@ -32,52 +32,43 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = T5ForConditionalGeneration.from_pretrained('t5-base').to(device)
 
 with tempfile.TemporaryDirectory() as temp_dir:
-    temp_file_path = os.path.join(temp_dir, 'train_data.json')
-    with open(temp_file_path, 'w+') as temp_file:
-        for filename in os.listdir('./datasets'):
-            if not re.search(r'validate-', filename):
-                with open(f'./datasets/{filename}', 'r') as f:
-                    try:
-                        for item in json.load(f):
-                            if 'input_text' in item and 'target_text' in item:
-                                temp_file.write(json.dumps(item) + '\n')
-                    except json.JSONDecodeError:
-                        print(f"Error loading file: {filename}")
-        temp_file.seek(0)
-        train_data = [json.loads(line) for line in temp_file]
+    train_encodings_file = tempfile.NamedTemporaryFile(mode='w+', dir=temp_dir, delete=False)
+    train_labels_file = tempfile.NamedTemporaryFile(mode='w+', dir=temp_dir, delete=False)
+    eval_encodings_file = tempfile.NamedTemporaryFile(mode='w+', dir=temp_dir, delete=False)
+    eval_labels_file = tempfile.NamedTemporaryFile(mode='w+', dir=temp_dir, delete=False)
 
-    train_encodings = tokenizer([item['input_text'] for item in train_data if isinstance(item['input_text'], str)],
-                                return_tensors='pt', padding=True,
-                                truncation=True)
-    train_labels = tokenizer([item['target_text'] for item in train_data if isinstance(item['target_text'], str)],
-                             return_tensors='pt', padding=True,
-                             truncation=True)
+    for filename in os.listdir('./datasets'):
+        if not re.search(r'validate-', filename):
+            with open(f'./datasets/{filename}', 'r') as f:
+                try:
+                    for item in json.load(f):
+                        if 'input_text' in item and 'target_text' in item:
+                            train_encodings_file.write(json.dumps(item['input_text']) + '\n')
+                            train_labels_file.write(json.dumps(item['target_text']) + '\n')
+                except json.JSONDecodeError:
+                    print(f"Error loading file: {filename}")
+
+        elif re.search(r'validate-', filename):
+            with open(f'./datasets/{filename}', 'r') as f:
+                try:
+                    for item in json.load(f):
+                        if 'input_text' in item and 'target_text' in item:
+                            eval_encodings_file.write(json.dumps(item['input_text']) + '\n')
+                            eval_labels_file.write(json.dumps(item['target_text']) + '\n')
+                except json.JSONDecodeError:
+                    print(f"Error loading file: {filename}")
+
+    train_encodings_file.seek(0)
+    train_labels_file.seek(0)
+    eval_encodings_file.seek(0)
+    eval_labels_file.seek(0)
+
+    train_encodings = tokenizer(train_encodings_file.readlines(), return_tensors='pt', padding=True, truncation=True)
+    train_labels = tokenizer(train_labels_file.readlines(), return_tensors='pt', padding=True, truncation=True)
+    eval_encodings = tokenizer(eval_encodings_file.readlines(), return_tensors='pt', padding=True, truncation=True)
+    eval_labels = tokenizer(eval_labels_file.readlines(), return_tensors='pt', padding=True, truncation=True)
 
     train_dataset = TextGenerationDataset(train_encodings, train_labels)
-
-    temp_file_path = os.path.join(temp_dir, 'eval_data.json')
-    with open(temp_file_path, 'w+') as temp_file:
-        for filename in os.listdir('./datasets'):
-            if re.search(r'validate-', filename):
-                with open(f'./datasets/{filename}', 'r') as f:
-                    try:
-                        for item in json.load(f):
-                            if 'input_text' in item and 'target_text' in item:
-                                temp_file.write(json.dumps(item) + '\n')
-                    except json.JSONDecodeError:
-                        print(f"Error loading file: {filename}")
-        temp_file.seek(0)
-        eval_data = [json.loads(line) for line in temp_file]
-
-    eval_encodings = tokenizer([item['input_text'] for item in eval_data if isinstance(item['input_text'], str)],
-                               return_tensors='pt', padding=True,
-                               truncation=True)
-    eval_labels = tokenizer([item['target_text'] for item in eval_data if isinstance(item['target_text'], str)],
-                            return_tensors='pt', padding=True,
-                            truncation=True)
-
-    # ----------------- Training -----------------
-
     eval_dataset = TextGenerationDataset(eval_encodings, eval_labels)
 
     eval_dataset_combined = ConcatDataset([train_dataset, eval_dataset])
@@ -86,9 +77,9 @@ with tempfile.TemporaryDirectory() as temp_dir:
     eval_dataloader = DataLoader(eval_dataset_combined, batch_size=8, shuffle=True)
 
     training_args = TrainingArguments(
-        output_dir='../results',
+        output_dir='./results',
         evaluation_strategy='epoch',
-        logging_dir='../logs',
+        logging_dir='./logs',
         per_device_train_batch_size=1,
         num_train_epochs=100,
     )
@@ -104,3 +95,13 @@ with tempfile.TemporaryDirectory() as temp_dir:
     trainer.train()
 
     model.save_pretrained('./trained_model')
+
+    train_encodings_file.close()
+    train_labels_file.close()
+    eval_encodings_file.close()
+    eval_labels_file.close()
+
+    os.unlink(train_encodings_file.name)
+    os.unlink(train_labels_file.name)
+    os.unlink(eval_encodings_file.name)
+    os.unlink(eval_labels_file.name)
