0301d3e22adf3d6d0ba95c209e19323ca6990211
Nguyen <68248147+Ne-k@users.noreply.github.com>
2023-06-06 00:27:16 -0700

    Update

--- src/trainer.py
+++ src/trainer.py
@@ -1,11 +1,10 @@
 import json
 import os
-import tempfile
-import torch
 import re
-from tqdm import tqdm, trange
-
+import sqlite3
+import torch
 from torch.utils.data import ConcatDataset, DataLoader
+from tqdm import tqdm
 from transformers import T5ForConditionalGeneration, T5Tokenizer, Trainer, TrainingArguments
 
 dataset_dir = "./datasets"
@@ -37,22 +36,26 @@ tokenizer = T5Tokenizer.from_pretrained('t5-base')
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = T5ForConditionalGeneration.from_pretrained('t5-base').to(device)
 
-with tempfile.NamedTemporaryFile(mode='w+', delete=True) as temp_file:
-    num_files = len([filename for filename in os.listdir(dataset_dir) if not filename.startswith('validate-')])
-    print(f"Uploading {num_files} dataset files")
-    for filename in tqdm(os.listdir(dataset_dir), desc="Uploading dataset files", total=num_files):
-        if not filename.startswith('validate-'):
-            with open(f'{dataset_dir}/{filename}', 'r') as f:
-                for item in json.load(f):
-                    if 'input_text' in item and 'target_text' in item:
-                        temp_file.write(json.dumps(item) + '\n')
-    temp_file.seek(0)
-    train_data = [json.loads(line) for line in temp_file]
+conn = sqlite3.connect('data.db')
+conn.execute('''CREATE TABLE IF NOT EXISTS data
+             (input_text TEXT NOT NULL,
+             target_text TEXT NOT NULL);''')
 
-train_encodings = tokenizer([item['input_text'] for item in train_data if isinstance(item['input_text'], str)],
+num_files = len([filename for filename in os.listdir(dataset_dir) if not filename.startswith('validate-')])
+print(f"Uploading {num_files} dataset files")
+for filename in tqdm(os.listdir(dataset_dir), desc="Uploading dataset files", total=num_files):
+    if not filename.startswith('validate-'):
+        with open(f'{dataset_dir}/{filename}', 'r') as f:
+            for item in json.load(f):
+                if 'input_text' in item and 'target_text' in item and item['target_text']:
+                    conn.execute("INSERT INTO data (input_text, target_text) VALUES (?, ?)", (item['input_text'], item['target_text']))
+
+train_data = conn.execute("SELECT * FROM data").fetchall()
+
+train_encodings = tokenizer([item[0] for item in train_data if isinstance(item[0], str)],
                             return_tensors='pt', padding=True,
                             truncation=True)
-train_labels = tokenizer([item['target_text'] for item in train_data if isinstance(item['target_text'], str)],
+train_labels = tokenizer([item[1] for item in train_data if isinstance(item[1], str)],
                          return_tensors='pt', padding=True,
                          truncation=True)
 
@@ -60,22 +63,22 @@ train_dataset = TextGenerationDataset(train_encodings, train_labels)
 
 del train_data
 
-with tempfile.NamedTemporaryFile(mode='w+', delete=True) as temp_file:
-    num_files = len([filename for filename in os.listdir(dataset_dir) if re.search(r'validate-', filename)])
-    print(f"Uploading {num_files} validation files")
-    for filename in tqdm(os.listdir(dataset_dir), desc="Uploading validation files", total=num_files):
-        if re.search(r'validate-', filename):
-            with open(f'{dataset_dir}/{filename}', 'r') as f:
-                for item in json.load(f):
-                    if 'input_text' in item and 'target_text' in item:
-                        temp_file.write(json.dumps(item) + '\n')
-    temp_file.seek(0)
-    eval_data = [json.loads(line) for line in temp_file]
+num_files = len([filename for filename in os.listdir(dataset_dir) if re.search(r'validate-', filename)])
+print(f"Uploading {num_files} validation files")
+for filename in tqdm(os.listdir(dataset_dir), desc="Uploading validation files", total=num_files):
+    if re.search(r'validate-', filename):
+        with open(f'{dataset_dir}/{filename}', 'r') as f:
+            for item in json.load(f):
+                if 'input_text' in item and 'target_text' in item and item['target_text']:
+                    conn.execute("INSERT INTO data (input_text, target_text) VALUES (?, ?)", (item['input_text'],
+                                                                                              item['target_text']))
 
-eval_encodings = tokenizer([item['input_text'] for item in eval_data if isinstance(item['input_text'], str)],
+eval_data = conn.execute("SELECT * FROM data").fetchall()
+
+eval_encodings = tokenizer([item[0] for item in eval_data if isinstance(item[0], str)],
                            return_tensors='pt', padding=True,
                            truncation=True)
-eval_labels = tokenizer([item['target_text'] for item in eval_data if isinstance(item['target_text'], str)],
+eval_labels = tokenizer([item[1] for item in eval_data if isinstance(item[1], str)],
                         return_tensors='pt', padding=True,
                         truncation=True)
 
@@ -90,11 +93,11 @@ training_args = TrainingArguments(
     evaluation_strategy='epoch',
     logging_dir='../logs',
     per_device_train_batch_size=1,
-    num_train_epochs=100,
+    num_train_epochs=2,
 )
 
-train_dataloader = DataLoader(train_dataset, batch_size=1, shuffle=True)
-eval_dataloader = DataLoader(eval_dataset_combined, batch_size=1, shuffle=True)
+train_dataloader = DataLoader(train_dataset, batch_size=1, shuffle=True, pin_memory=True)
+eval_dataloader = DataLoader(eval_dataset_combined, batch_size=1, shuffle=True, pin_memory=True)
 
 print("\033[93mStarting training...\033[0m")
 trainer = Trainer(
@@ -110,3 +113,5 @@ with torch.no_grad():
 trainer.train()
 
 model.save_pretrained('../trained_model')
+
+conn.close()
