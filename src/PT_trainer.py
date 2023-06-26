e2d20a624a4846a1f2b35387db5df2385f0e67f5
Nguyen <68248147+Ne-k@users.noreply.github.com>
2023-06-26 13:26:56 -0700

    Updated I guess

--- src/PT_trainer.py
+++ src/PT_trainer.py
@@ -6,6 +6,7 @@ import torch
 from torch.utils.data import ConcatDataset, DataLoader
 from tqdm import tqdm
 from transformers import T5ForConditionalGeneration, T5Tokenizer, Trainer, TrainingArguments
+from tokenizers import ByteLevelBPETokenizer
 
 dataset_dir = "./datasets"
 
@@ -24,6 +25,8 @@ class TextGenerationDataset(torch.utils.data.Dataset):
         return len(self.labels['input_ids'])
 
 
+conn = sqlite3.connect('data.db')
+
 if not torch.cuda.is_available():
     print("Cuda is not available, training will be slow without cuda, switching to cpu")
 else:
@@ -31,12 +34,13 @@ else:
 
 torch.cuda.empty_cache()
 print("Memory cache cleared")
+conn.execute("DROP TABLE IF EXISTS data")
+print("Database cleared")
 
 tokenizer = T5Tokenizer.from_pretrained('google/flan-t5-large')
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = T5ForConditionalGeneration.from_pretrained('google/flan-t5-large').to(device)
 
-conn = sqlite3.connect('data.db')
 conn.execute('''CREATE TABLE IF NOT EXISTS data
              (input_text TEXT NOT NULL,
              target_text TEXT NOT NULL);''')
@@ -49,22 +53,33 @@ for filename in tqdm(os.listdir(dataset_dir), desc="Uploading dataset files", to
             try:
                 for item in json.load(f):
                     if 'input_text' in item and 'target_text' in item and item['target_text']:
-                        conn.execute("INSERT INTO data (input_text, target_text) VALUES (?, ?)", (item['input_text'], item['target_text']))
+                        conn.execute("INSERT INTO data (input_text, target_text) VALUES (?, ?)",
+                                     (item['input_text'], item['target_text']))
             except json.JSONDecodeError:
                 print(f"Error: Failed to decode JSON in file {filename}")
 
-train_data = conn.execute("SELECT * FROM data").fetchall()
+batch_size = 1000
 
-train_encodings = tokenizer([item[0] for item in train_data if isinstance(item[0], str)],
-                            return_tensors='pt', padding=True,
-                            truncation=True)
-train_labels = tokenizer([item[1] for item in train_data if isinstance(item[1], str)],
-                         return_tensors='pt', padding=True,
-                         truncation=True)
+def data_generator():
+    cursor = conn.cursor()
+    try:
+        cursor.execute("SELECT COUNT(*) FROM data")
+        num_rows = cursor.fetchone()[0]
 
-train_dataset = TextGenerationDataset(train_encodings, train_labels)
+        for offset in range(0, num_rows, batch_size):
+            cursor.execute(f"SELECT * FROM data LIMIT {batch_size} OFFSET {offset}")
+            rows = cursor.fetchall()
+            for row in rows:
+                yield row
+    finally:
+        cursor.close()
 
-del train_data
+tokenizer = ByteLevelBPETokenizer()
+
+train_encodings = tokenizer.encode_batch([item[0] for item in data_generator() if isinstance(item[0], str)])
+train_labels = tokenizer.encode_batch([item[1] for item in data_generator() if isinstance(item[1], str)])
+
+
 
 num_files = len([filename for filename in os.listdir(dataset_dir) if re.search(r'validate-', filename)])
 print(f"Uploading {num_files} validation files")
